915ee666aacdb8c2116f9accde1d8beec0523732
Abdulaziz-Mirsagatov <mirsagah@mail.uc.edu>
2023-06-24 16:03:38 -0400

    BrowserRouter changed to StaticRouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { StaticRouter } from "react-router-dom/server";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -11,9 +11,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <StaticRouter>
         <App />
-      </BrowserRouter>
+      </StaticRouter>
     </Provider>
   </React.StrictMode>
 );
