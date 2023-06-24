85cda6f93e98ad72dcae485f79d1c0594900500d
Abdulaziz-Mirsagatov <mirsagah@mail.uc.edu>
2023-06-24 16:15:09 -0400

    Reverted back to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
-import { StaticRouter } from "react-router-dom/server";
+import { BrowserRouter } from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -11,9 +11,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <StaticRouter>
+      <BrowserRouter>
         <App />
-      </StaticRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
